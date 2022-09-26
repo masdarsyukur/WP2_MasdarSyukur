9b0a0fd8d3e77bc7ae5b95a5cf699ea01d42f8a7
masdarsyukur <masdarsyukur97@gmail.com>
2022-09-26 23:59:55 +0700

    contoh 2 dan 3

--- application/controllers/latihan1.php
+++ application/controllers/latihan1.php
@@ -1,8 +1,17 @@
-<?php  
-class latihan1 extends C1_Controller
+<?php
+class latihan1 extends CI_Controller
 {
     public function index()
     {
-        Echo "saya sedang belajar web programing, Ternyata belajar web programing itu mengasyikan";
+        echo "Selamat Datang....selamat belajar Web Programing";
+        //$this->load->view('view-latihan1');
+    }
+
+    public function penjumlahan($n1, $n2)
+    {
+        $this->load->model('Model_Latihan1');
+        $data['nilai1'] = $n1;
+        $data['nilai2']
+        $data['hasil'] = $this->Model_latihan1'>jumlah($n1, $n2);
     }
 }
---
+++ application/views/view-latihan.php
@@ -0,0 +1,17 @@
+<html>
+
+<head>
+    <title>Latihan 1</title>
+</head>
+
+<body>
+    Halo Kawan ... Yuk kita belajar web Programing..!!!<br>
+    Nilai 1 = <?= $nilai; ?>
+    Nilai 2 = <?= $nilai; ?>
+    ini hasil dari pemodelan dengan methode penjumlah yaitu <?=
+                                                            $nilai . " + " . $nilai . " = " . $hasil; ?>
+
+</body>
+
+
+</html>
